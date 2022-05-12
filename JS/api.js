6ba16554aa6accbf27a067d84fc657d76214e290
Lucas <lucasbasoaltocontreras@gmail.com>
2022-05-12 10:16:51 -0400

    ingreso de datos a catalogo

---
+++ JS/api.js
@@ -0,0 +1,21 @@
+async function start(){
+    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    const data = await response.json()
+    createBreedList(data.message)
+
+}
+
+start()
+
+
+function createBreedList(breedList){
+    document.getElementById('breed').innerHTML =
+    `
+    <select name="" id="">
+      <option>Elija una raza de perro</option>
+      ${Object.keys(breedList).map(function (breed){
+        return `<option>${breed}</option>`
+       }).join('')}
+    </select>
+    `
+}
